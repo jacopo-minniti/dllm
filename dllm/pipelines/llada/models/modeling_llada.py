f2a38b8a70b69525a51d3e3e4526e120e72ed352
Jacopo Minniti <jacopo.minniti004@gmail.com>
2026-03-20 19:42:53 -0300

    configs organization

--- dllm/pipelines/llada/models/modeling_llada.py
+++ dllm/pipelines/llada/models/modeling_llada.py
@@ -175,7 +175,7 @@ def activation_checkpoint_function(cfg: ModelConfig):
     return partial(
         checkpoint,
         preserve_rng_state=preserve_rng_state,
-        use_reentrant=False,
+        use_reentrant=True,
     )
 
 
